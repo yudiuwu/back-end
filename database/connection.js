4f12fc7a95780932aeda4434eaaf915e36e2fdd0
yudiu <yudi.hartmann@rede.ulbra.br>
2026-06-28 15:31:49 -0300

    Delete database directory

--- database/connection.js
+++
@@ -1,9 +0,0 @@
-const knex = require('knex');
-const config = require('../knexfile');
-
-// Puxa a configuração de "development" do knexfile
-const connection = knex(config.development);
-
-module.exports = connection;
-
-
